69e1218235208f430b26a993c10ed531fa210cc8
= <=>
2016-01-20 12:07:54 +0100

    Added add function

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const Customers = new React.createClass({
     getInitialState: function() {
         return {
             customers: [
-                {"id": 0, "name": "Daniel Gomez", "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros."},
-                {"id": 1, "name": "Emil Haugberg van Veen", "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros."},
-                {"id": 2, "name": "Luca Verhees", "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros."}
+                {"id": 0, "name": "Daniel Gomez", "message": "UI nerd"},
+                {"id": 1, "name": "Emil Haugberg van Veen", "message": "UI nerd"},
+                {"id": 2, "name": "Luca Verhees", "message": "FP nerd"}
             ]
         }
     },
 
-    add: function() {
-        console.log({"name": "test", "message": "test"});
+    add: function(object) {
+        var state = this.state.customers;
+        var id = state.length;
+
+        object.id = id;
+
+        state.push(object);
+
+        this.setState(state);
     }
     ,
     render: function() {
@@ -19,7 +26,6 @@ const Customers = new React.createClass({
             <div>
                 {customers.map(x => <CustomerMessage key={x.id} name={x.name} message={x.message} /> )}
                 <NewCustomer add={this.add} />
-                <button>add</button>
             </div>
         )
     }
@@ -42,13 +48,35 @@ const CustomerMessage = new React.createClass({
 });
 
 const NewCustomer = new React.createClass({
+    getInitialState: function() {
+      return {name: "", message: ""};
+    },
+
+    handleNameChange: function(event) {
+        this.setState({name: event.target.value});
+    }
+    ,
+    handleMessageChange: function(event) {
+        this.setState({message: event.target.value});
+    }
+    ,
+    handleAdd: function() {
+        var empty = this.state.name == "" || this.state.message == "" ? true : false;
+
+        if(!empty) {
+            this.props.add(this.state);
+        }
+    }
+
+    ,
     render: function() {
         return (
             <div>
                 <div className="add">
                     <div className="user">
-                        <NameInput />
-                        <MessageInput />
+                        <input type="text" placeholder="name" value={this.state.value} onChange={this.handleNameChange}/>
+                        <input type="text" placeholder="message" value={this.state.value} onChange={this.handleMessageChange} />
+                        <button onClick={this.handleAdd}>add</button>
                     </div>
                 </div>
             </div>
